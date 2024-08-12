a43fb733e3f0a4b1f0ff4642840ae6777b5e1c7e
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-08-12 23:42:28 +0530

    create utility functions for handling localstorage and api requests

---
+++ src/utils/index.js
@@ -0,0 +1,61 @@
+// A class that provides utility functions for working with local storage
+export class LocalStorage {
+  // Get a value from local storage by key
+  static get(key) {
+    if (typeof window === "undefined") return;
+    const value = localStorage.getItem(key);
+    if (value) {
+      try {
+        return JSON.parse(value);
+      } catch (err) {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  // Set a value in local storage by key
+  static set(key, value) {
+    if (typeof window === "undefined") return;
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  // Remove a value from local storage by key
+  static remove(key) {
+    if (typeof window === "undefined") return;
+    localStorage.removeItem(key);
+  }
+
+  // Clear all items from local storage
+  static clear() {
+    if (typeof window === "undefined") return;
+    localStorage.clear();
+  }
+}
+
+// A utility function for handling API requests with loading, success, and error handling
+export const requestHandler = async (api, setLoading, onSuccess, onError) => {
+  // Show loading state if setLoading function is provided
+  if (setLoading) setLoading(true);
+
+  try {
+    // Make the API request
+    const response = await api();
+    const { data } = response;
+
+    if (data?.success) {
+      // Call the onSuccess callback with the response data
+      onSuccess(data);
+    }
+  } catch (error) {
+    // Handle error cases, including unauthorized and forbidden cases
+    if ([401, 403].includes(error?.response?.data?.statusCode)) {
+      localStorage.clear(); // Clear local storage on authentication issues
+      if (typeof window !== "undefined") window.location.href = "/login"; // Redirect to login page
+    }
+    onError(error?.response?.data?.message || "Something went wrong");
+  } finally {
+    // Hide loading state if setLoading function is provided
+    if (setLoading) setLoading(false);
+  }
+};
