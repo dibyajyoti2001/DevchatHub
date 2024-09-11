5b746d4927b3b48261314ceb5c0d2ffe43cb2b3d
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-09-11 12:28:56 +0530

    create register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,25 @@
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import Input from "../components/Input.jsx";
+import Button from "../components/Button.jsx";
+
+export default function Register() {
+  return (
+    <div className="flex justify-center items-center flex-col h-screen w-screen">
+      <div className="w-1/3 p-8 flex justify-center items-center gap-5 flex-col bg-gray-900 shadow-md rounded-2xl my-16 border-primary border-[1px]">
+        <h1 className="inline-flex items-center text-2xl mb-4 flex-col">
+          <LockClosedIcon className="h-8 w-8 mb-2" /> Register
+        </h1>
+        <Input placeholder="Enter the email..." type="email" />
+        <Input placeholder="Enter the username..." type="text" />
+        <Input placeholder="Enter the password..." type="password" />
+        <Button fullWidth>Register</Button>
+        <small className="text-zinc-300">
+          Already have an account?{" "}
+          <a className="text-primary hover:underline" href="/login">
+            Login
+          </a>
+        </small>
+      </div>
+    </div>
+  );
+}
