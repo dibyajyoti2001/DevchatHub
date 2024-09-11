eabd64eeb776d43fe54aaf13db6fe466a8b286f7
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-09-11 12:26:49 +0530

    add the redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ReduxProvider from "./store/provider.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 
